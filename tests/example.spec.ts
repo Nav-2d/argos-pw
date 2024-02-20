57b0631f25f4a502b43eacea36b2a193233596b1
nav-2d <navdeep.singh2088@gmail.com>
2024-02-19 21:40:19 -0800

    run on firefox and webkit as well

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,3 +5,9 @@ test('screenshot homepage', async ({ page }) => {
   await page.goto('https://argos-ci.com/docs/why-argos');
   await argosScreenshot(page, 'homepage');
 });
+
+test('screenshot pages', async ({ page }, testInfo) => {
+  const browserName = testInfo.project.name;
+  await page.goto('https://playwright.dev/');
+  await argosScreenshot(page, `pw-home-${browserName}`);
+});
